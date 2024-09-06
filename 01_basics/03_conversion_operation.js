ec02502bd8c52437e60a7b73170f15e4eb041e85
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2024-09-06 10:21:32 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,73 @@
+// let score ="33"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+
+// let score ="33abcd"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// when there is string in which is the combination of integer and alphabets , when we convert that string to a number then it is convertible
+// to a number but it's value is NaN(not a number)
+
+// let score =null
+// console.log(score);
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+//
+ /*
+ null=> data_type ---objects
+ when null is converted into number then its value become 0 and its data_type is number rather then object
+ */
+
+
+//  let score =undefined;
+// console.log(score);
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// after conversion of undefined into a number its datatype becomes number and its value becomes not a number
+
+
+
+// let score =true;
+// console.log(score);
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score ="priya";
+// console.log(score);
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+ //when there exist a value such that it doesn't convertible into a number then its value becomes NOT A NUMBER but its datatype is still a number
+
+
+
+ // CONVERSION OF BOOLEAN VALUE(0/1) TO BOOLEAN VALUE(TRUE/FALSE)
+//   let isLoggedIn=1
+//    let booleanIsLoggedIn=Boolean(isLoggedIn)
+//    console.log(booleanIsLoggedIn);
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);// this will give false => empty string is treated as 0;
+// non empty string is treated as 1 hence its conersion into boolean give true 
+
+
+ let score =33
+ let string_number=String(score);
+ console.log(string_number)
+ console.log(typeof string_number)
