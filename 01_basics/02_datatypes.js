806ddffbd301da322b12aef84709e9a37d3d3029
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2024-09-06 09:30:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//"use strict";//treat all js code as the newer version of js
+// alert(3+5);// we are using nodejs not browser
+let name="priya"
+let age=22;
+let isLoggedIn=true;
+let state;
+
+
+/*
+
+datatypes=>
+    number=> 2 to the power 8
+   bigint =>it is used to store the big numbers
+   strings=> strings are written in the double quotes
+   boolean=>true/false
+   //null=> it is stand alone value in the js and also a datatype// data_type of " null is an object"
+   undefined=>// it datatype is undefined 
+    symbol=> it is used to create a unique constant which is are useful when creating APIs
+
+   object
+*/
+// console.log(typeof "priya");
+// console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
