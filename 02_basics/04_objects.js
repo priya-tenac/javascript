f7e6140e27429ed1433d160fc8c303a9597181bd
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2024-09-10 06:30:55 +0000

    04_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,10 +29,58 @@ const tinderUser={}// this 1 and 2 declaration is same => give empty object {} a
  const obj1={1:"a",2:"b"}
  const obj2={3:"m",4:"n"}
 const obj3={...obj1,...obj2}
-console.log(obj3)
+// console.log(obj3)
 const obj4={obj1,obj2}
-console.log(obj4)
+// console.log(obj4)
 const obj5=Object.assign(obj1,obj2)
 const obj6=Object.assign({},obj1,obj2);// {}=> target and other objects are act as sources
-console.log(obj5)
-console.log(obj6);
+// console.log(obj5)
+// console.log(obj6);
+const user=[
+    { 1:"a",
+        email:"abs@google.com"
+    },
+    {},
+    {
+
+    }
+ ]
+ user[1].email
+//  console.log(tinderUser);
+//  console.log(Object.keys(tinderUser))// this will give output as array from the objects
+//  console.log(Object.values(tinderUser))
+//  console.log(Object.entries(tinderUser))// this will give array within the array as output=> a pair of key and values gives array
+
+//  console.log(tinderUser.hasOwnProperty('isloggedin'))
+//  console.log(tinderUser.hasOwnProperty('islogged'))
+
+ // DESTRUCTURING OF OBJECTS
+  const course={
+    course_name:" javascript",
+    instructor_name:"hitesh",
+    course_price:"999"
+  }
+
+  //course.instructor_name
+// when we have to access the same value at multiple time , rather than using "." notation to access the value , use this below method to access the same value at multiple time
+
+// this is desturcturing of the object
+//   const{instructor_name}=course
+//   console.log(instructor_name);
+  // OR
+   const{instructor_name:instructor}=course
+   console.log(instructor)
+   // props of react;
+   // props stands for properties which is used to passing thedata from one components to other components
+
+    // const navbar=(props.company)=>{
+    // }
+    // navbar(company="priya")
+
+    // rahter than use props.company use only company as in the argument in the react => which is destructuring of the object
+   // write company as argument , write company within the { } as we destructuring objects
+    // const navbar=({company})=>{
+
+    // }
+    // navbar(company="savya")
+    
