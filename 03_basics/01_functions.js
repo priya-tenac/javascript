ac291f58a4ff353e6f0a237c66373a5de21af386
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2024-09-10 08:27:55 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+// function definition
+
+// function say_my_name(){
+//     console.log("p")
+//     console.log("r")
+//     console.log("i")
+//     console.log("y")
+//     console.log("a")
+// }
+//say_my_name=> function reference
+// say_my_name()// function execution/function call
+// function add_two_numbers(number1,number2){
+//     console.log(number1+number2)
+// }
+// // add_two_numbers(4,5);
+// // add_two_numbers(7,"a")
+// // add_two_numbers(2,"6")
+// // add_two_numbers(8,null)
+//  const result=add_two_numbers(5,5)
+//  console.log(`the result is: ${result}`)//what add_two_numbers return  in the after execution program=> this will give undefined , that means it doesn't return anything
+
+
+// function add_two_numbers(number1,number2){
+// let result=number1+number2
+// return result
+// console.log("priya")
+// }
+
+// function add_two_numbers(number1,number2){
+//  return number1+number2
+// }
+
+
+//  const result=add_two_numbers(5,5)
+//  console.log(`the result is: ${result}`)//what add_two_numbers return  in the after execution program=> this will give undefined , that means it doesn't return anything
+
