946a17fa93706370e93cc386af9f984b80564f21
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2024-09-16 16:34:17 +0000

    while_do

---
+++ 05_iterations/02_while_do.js
@@ -0,0 +1,20 @@
+let index=1
+while(index<=10)
+{
+//    console.log(`value of the index is ${index}`);
+   index=index+2
+}
+
+let my_array=["batman","flash","thor","superman"]
+let arr=0
+while(arr<my_array.length)
+{
+    // console.log(`value is ${my_array[arr]}.`)
+    arr++;
+}
+ let score=11;
+
+do {
+    console.log(`score is: ${score}`)
+    score++;
+} while (score<=10);
