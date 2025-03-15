b5181548c882695323aec9b5900d876d2da1f615
Priya Chauhan <128723652+priya-tenac@users.noreply.github.com>
2025-03-15 11:13:53 +0000

    math_pi

--- 10_oops/10_math_pie.js
+++ 10_oops/10_math_pie.js
@@ -3,7 +3,7 @@
 // console.log(Math.PI)// the value of pi is unchangeble
 //reason we cant change the object value
 const pi=Object.getOwnPropertyDescriptor(Math,"PI")
-console.log(pi)
+// console.log(pi)
 
 // {
 //     value: 3.141592653589793,
@@ -14,5 +14,66 @@ console.log(pi)
 
 // this kind of property which is not changeble we can define also.
 
+// 1st method to create an object
+// const my_new_object=Object.create(null)// by default, it value is null 
 
-const my_new_object=Object.create(null)// by default, it value is null 
+// 2nd method to  an create object
+
+// const chai={
+//     name:"ginger tea",
+//     price:"250",
+//     is_available: true
+// }
+// console.log(chai);
+// console.log(Object.getOwnPropertyDescriptor(chai))// this will give undefined as we doesn't pass the argument for which we have to get the descriptor
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+
+// property definition inside the object
+// Object.defineProperty(chai,'name',{
+//     writable:false,
+//     enumerable: false,
+//     configurable:false
+// })
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+// for (let [key,value] of chai) {
+// console.log(`${key} : ${value}`);    
+// }
+// here the object "chai" is not iterable directly depends on situation(use "object.entries(object_name)"  to iterate the object)
+
+// for (let [key,value] of Object.entries(chai)) {
+//     console.log(`${key} : ${value}`);    
+//     }
+
+// prevent the object from code bursting use this code 
+const chai={
+    name:"ginger tea",
+    price:"250",
+    is_available: true,
+    orderchai:function(){
+        console.log(`chai nhi bani(code bursting)`)
+    }
+}
+// for (let [key,value] of Object.entries(chai)) {
+//     console.log(`${key} : ${value}`);    
+//     }
+// handling the code bursting(as we want only key,value pair in the object)
+// for (let [key,value] of Object.entries(chai)) {
+   
+//     if (typeof value!=='function') {
+//         console.log(`${key} : ${value}`);  
+//     } 
+//     }
+    // if we want the property that there is no iteration possible with object make the "enumeration:false" in the definition of the object 
+   // name is th eout of the iterable loop hence in the output name key,value pair doesn't show
+    Object.defineProperty(chai,'name',{
+        enumerable:false
+    })
+
+    for (let [key,value] of Object.entries(chai)) {
+   
+        if (typeof value!=='function') {
+            console.log(`${key} : ${value}`);  
+        } 
+        }
